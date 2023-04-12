b455681e73b6156c7abf8ee369b4431149d66a12
Timothy Ryon <tim@timryon.com>
2023-04-12 14:32:41 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield"
+];
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    return [...cats, name]
+}
+function prependCat(name) {
+    return [name, ...cats]
+}
+function removeLastCat() {
+    return [...cats.slice(0,2)]
+}
+function removeFirstCat() {
+    return [...cats.slice(1)]
+}
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+
+
+
+
+/*
+const cats = [
+    "Mr Bigglesworth"
+];
+*/
+
